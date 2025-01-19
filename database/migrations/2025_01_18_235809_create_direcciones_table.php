196127d72738126b9aad95033bdcc247a00d2291
Angel Montes <angelmontes963@gmail.com>
2025-01-18 18:13:04 -0600

    Creacion de migraciones  Poblacion Temporal  Personas  Telefonos  Direcciones

---
+++ database/migrations/2025_01_18_235809_create_direcciones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('direcciones', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->text('calle');
+            $table->text('numero_exterior');
+            $table->text('numero_interior')->nullable();
+            $table->text('colonia');
+            $table->integer('cp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('direcciones');
+    }
+};
